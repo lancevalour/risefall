6365a9485ea2671f702dc5c33a86190f0f088e53
Yicheng Zhang <zhangyicheng1234@gmail.com>
2016-03-14 20:02:10 -0400

    Finish basic function

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-id = 0;
+var id = 0;
 
 var timer;
 var time;
+
+var windowID = 0;
 
 var windowWidth = 800;
 var windowHeight = 600;
@@ -17,10 +19,10 @@ function startTimer() {
     time = new Date();
     console.log(time.getHours());
     console.log(time.getMinutes());
-    if (7 < time.getHours() < 19) {
+    if (7 < time.getHours() < 19 && time.getMinutes() == 59) {
         // if (time.getMilliseconds() == 59 {
-        clearInterval(timer);
-        launch();
+            clearInterval(timer);
+            launch();
         // }
         return 0;
     }
@@ -30,45 +32,80 @@ function startTimer() {
 
 
 function launch() {
+    setNotificationListener();
+
     notify();
 
     setInterval(function() {
         console.log("launch notify timer");
         notify();
-    }, 1000 * 10);
+    }, 1000 * 60 * 60);
 
 }
 
 function notify() {
     var timeStr = time.getFullYear() + "-" + (time.getMonth() + 1) +
-        "-" + time.getDate() + " " + time.getHours() + ":" +
-        time.getMinutes();
+    "-" + time.getDate() + " " + time.getHours() + ":" +
+    time.getMinutes();
 
-    chrome.notifications.create("id" + id++, {
-        type: "basic",
-        title: timeStr,
-        message: "Time to add energy cycle.",
-        iconUrl: "icon_128.png"
-    }, function() {
+    // chrome.notifications.create("id" + id++, {
+    //     type: "basic",
+    //     title: timeStr,
+    //     message: "Time to add energy cycle.",
+    //     iconUrl: "icon_128.png"
+    // }, creationCallback);
+    open();
 
-    });
+}
 
-    chrome.notifications.onClicked.addListener(function(notificationId) {
-        open(notificationId);
+function setNotificationListener(){
+    chrome.notifications.onClicked.addListener(function() {
+        open();
     });
 }
 
-function open(windowId) {
 
 
-    chrome.app.window.create('window.html', {
-        'id': windowId,
-        outerBounds: { // 'bounds' is deprecated, and you want full window size
-            width: windowWidth,
-            height: windowHeight,
-            left: screen.availWidth / 2 - windowWidth / 2,
-            top: screen.availHeight / 2 - windowHeight / 2,
-        }
+function creationCallback(id) {
+    setTimeout(function() {
+        chrome.notifications.clear(id, function(wasCleared) {
 
-    });
+        });
+    }, 5000);
+
+}
+
+
+function open() {
+
+   chrome.windows.create({
+    'url': getSheetUrl(),
+    'width': windowWidth,
+    'height': windowHeight,
+    'left': Math.round(screen.availWidth / 2 - windowWidth / 2),
+    'top': Math.round(screen.availHeight / 2 - windowHeight / 2)
+});
+
+    // chrome.windows.get(windowID, function(chromeWindow) {
+    //     if (!chrome.runtime.lastError && chromeWindow) {
+    //         chrome.windows.update(windowID, { focused: true });
+    //         return;
+    //     }
+    //     chrome.windows.create({
+    //         'url': getSheetUrl(),
+    //         'width': windowWidth,
+    //         'height': windowHeight,
+    //         'left': Math.round(screen.availWidth / 2 - windowWidth / 2),
+    //         'top': Math.round(screen.availHeight / 2 - windowHeight / 2)
+    //     },
+    //     function(chromeWindow) {
+    //         windowID = chromeWindow.id;
+    //     }
+    //     );
+    // });
+
+}
+
+function getSheetUrl(){
+    return "https://docs.google.com/spreadsheets/d/1GE--PHt2vNHuqK9hy_1voocdjrhH9X4cEeGwePPIhGE/edit#gid=0";
 }
